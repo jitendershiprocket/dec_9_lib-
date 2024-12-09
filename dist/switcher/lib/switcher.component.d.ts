6988b5a54f25d3b15a81818785a971bd396bb199
jitendershiprocket <jitender.sharma@kartrocket.com>
2024-12-09 19:00:41 +0530

    sdklfj

--- dist/switcher/lib/switcher.component.d.ts
+++
@@ -1,7 +0,0 @@
-import { OnInit } from '@angular/core';
-import * as i0 from "@angular/core";
-export declare class SwitcherComponent implements OnInit {
-    ngOnInit(): void;
-    static ɵfac: i0.ɵɵFactoryDeclaration<SwitcherComponent, never>;
-    static ɵcmp: i0.ɵɵComponentDeclaration<SwitcherComponent, "lib-switcher", never, {}, {}, never, never>;
-}
